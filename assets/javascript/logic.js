3eb6a1aa61047a07b8beba8c3e8ada547fb56413
2mlcmonkeys <2mlcmonkeys@gmail.com>
2019-06-30 10:32:50 -0400

    started adding html framework/css styling/ added images

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAc1j9joAsp-tHjmKDCmA4QA-I1mKIhVGo",
+    authDomain: "train-times-6395a.firebaseapp.com",
+    databaseURL: "https://train-times-6395a.firebaseio.com",
+    projectId: "train-times-6395a",
+    storageBucket: "",
+    messagingSenderId: "966917918482",
+    appId: "1:966917918482:web:c10d88407d54a01d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
